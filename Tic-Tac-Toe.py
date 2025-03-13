a02baf30da1d5c2152283dcb501fd063e65b38c8
Panshul goyal <panshulgoyal20+GitHub@gmail.com>
2025-03-13 16:24:21 +0530

    Update Tic-Tac-Toe.py

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -1,11 +1,14 @@
+import random as r      # Import Necessary To Play with Computer
+
 # Tic-Tac-Tac game
 def TicTacToe():
-    def TTT():
-        def sum(x , y , z):    # A Function To Add More Than Two Values
+    def TTT(Mode:int):                                                  # The Main Game Function Which Takes Mode as Input
+        
+        def sum(x , y , z):                                             # A Function To Add More Than Two Values
             return x + y + z
-            
-        # Lists to Set X and O
-        Xsheet = [0 , 0 , 0 , 0 , 0 , 0 ,0 , 0 ,0]      
+        
+        # Lists to Set X and O    
+        Xsheet = [0 , 0 , 0 , 0 , 0 , 0 ,0 , 0 ,0]
         Osheet = [0 , 0 , 0 , 0 , 0 , 0 ,0 , 0 ,0]
         turn = 1
 
@@ -38,70 +41,88 @@ def TicTacToe():
             ''')
 
         # Checks The Result Of the Game
-        def checkResult():
-            playing_board()
-            Wins = [[0 ,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
+        def checkwin():
+            Wins = [[0 ,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]    # All Possibilities To Win
             
             for win in Wins:
-                if sum(Xsheet[win[0]] , Xsheet[win[1]] , Xsheet[win[2]]) == 3:
+                if sum(Xsheet[win[0]] , Xsheet[win[1]] , Xsheet[win[2]]) == 3:      # Checks If X wins
                     print(f"X wins!")
                     return 1
-                elif sum(Osheet[win[0]] , Osheet[win[1]] , Osheet[win[2]]) == 3:
+                elif sum(Osheet[win[0]] , Osheet[win[1]] , Osheet[win[2]]) == 3:    # Checks If O wins
                     print(f"O wins!")
                     return 0
-            IfDraw = [o + x for o, x in zip(Osheet, Xsheet)]
-            if IfDraw == [1, 1, 1, 1, 1, 1, 1, 1, 1]:
+            XOsheet = [o + x for o, x in zip(Osheet, Xsheet)]                       # Checks The Entire Sheet
+            if XOsheet == [1, 1, 1, 1, 1, 1, 1, 1, 1]:                              # Checks If The Game is a Draw
                 print("Match is a Draw")
                 return 2
             return -1
-                
-        # Game Starts
+                 
+        # Game Starts    
         while True:
             # Shows The Game Board
             playing_board()
+            
             # X has First Turn
             if turn == 1:
                 print("X's turn")
                 try:
-                    choice = int(input("Enter the position: ")) # Takes The Position
-                    if choice < 0 or choice > 8:            # Checks If The Position Is Valid
+                    choice = int(input("Enter the position: "))                     # Takes The Position
+                    if choice < 0 or choice > 8:                                    # Checks If The Position Is Valid
                         print("Please Enter The Correct Position")
+                        turn = 0
                     else:
-                        if Xsheet[choice] == 1 or Osheet[choice] == 1:  # Check If The Position Is Already Taken
+                        if Xsheet[choice] == 1 or Osheet[choice] == 1:              # Checks If The Position Is Already Taken
                             print("Position already taken")
                             turn = 0
                         else:
-                            Xsheet[choice] = 1                           # Insert X In The Position
+                            Xsheet[choice] = 1                                      # Insert X In The Position
                 except:
                     print("Invalid input")
             else:
                 print("O's turn")
-                try:
-                    choice = int(input("Enter the position: "))     # Takes The Position
-                    if choice < 0 or choice > 8:                    # Checks If The Position Is Valid
-                        print("Please Enter The Correct Position")
-                    else:
-                        if Xsheet[choice] == 1 or Osheet[choice] == 1:      # Check If The Position Is Already Taken
-                            print("Position already taken")
+                if Mode == 1:                                                       # Checks Which Mode is Selected
+                    positions = []                                                  # All Available positions
+                    XOsheet = [o + x for o, x in zip(Osheet, Xsheet)]               # Entire Sheet
+                    for i in range(len(XOsheet)):
+                        if XOsheet[i] == 0:                                         # Checks For Empty Positions
+                            positions.append(i)
+                    choice = r.choice(positions)                                    # Chooses Positions Using Ramdom
+                    Osheet[choice] = 1                                              # Insert O In The Position 
+                    print(f"Computer chose {choice}")
+                else:
+                    try:
+                        choice = int(input("Enter the position: "))                 # Takes The Position
+                        if choice < 0 or choice > 8:                                # Checks If The Position Is Valid
+                            print("Please Enter The Correct Position")
                             turn = 1
                         else:
-                            Osheet[choice] = 1                          # Insert O In The Position
-                except:
-                    print("Invalid input")
+                            if Xsheet[choice] == 1 or Osheet[choice] == 1:          # Check If The Position Is Already Taken
+                                print("Position already taken")
+                                turn = 1
+                            else:
+                                Osheet[choice] = 1                                  # Insert O In The Position
+                    except:
+                        print("Invalid input")
                 
             try:
-                winner = checkResult()
-                if winner != -1:        # Breaks If The Result Is Decided
+                winner = checkwin()
+                if winner != -1:                                                    # Breaks If The Result Is Decided
+                    playing_board()
                     break
             except Exception as e:
                 print(e)
-            
-            turn = 1 if turn == 0 else 0        # Switches The Turn
+                
+            turn = 1 if turn == 0 else 0                                            # Switches The Turn
+                    
     while True:
-        TTT()       # Calls The Game Function
+        try:
+            Mode = int(input("What Mode Would you like to play? (For Single Player Type 1 /For Double Player Type 2): ").strip())   # Prompts The User To Select Game Mode
+        except:
+            print("Invalid input")
+        TTT(Mode)                                                                   # Calls The Game Function and Tells Thd Mode chosen
         replay = input("Do you want to play again? (yes/no): ").strip().lower()     # Asks If The Player Wants To Play Again
         play_again = ["yes", "y" , "yeah", "yup", "sure", "ok", "okay","yea", "ya", "yep"]
-        if replay not in play_again:            # Breaks If The Player Does Not Want To Play Again
+        if replay not in play_again:                                                # Breaks If The Player Does Not Want To Play Again
             print("Thanks for playing! Goodbye!")
             break
         
